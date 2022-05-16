2ab2a7b6186d44600ec1bbf523bfb197e06c7742
iuoily <1427718414@qq.com>
2022-05-16 22:47:49 +0800

    完成商品分类管理的所有功能

---
+++ src/com/taobaby/pojo/CarouselFigure.java
@@ -0,0 +1,36 @@
+package com.taobaby.pojo;
+
+public class CarouselFigure {
+
+  private String id;
+  private String url;
+  private Integer sequenceNum;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+
+  public Integer getSequenceNum() {
+    return sequenceNum;
+  }
+
+  public void setSequenceNum(Integer sequenceNum) {
+    this.sequenceNum = sequenceNum;
+  }
+
+}
